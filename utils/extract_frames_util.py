275cff4d86be1cead8139a7c70a04f7c36ef1d71
Mahmoud Taha <mahmoud.taha17@feng.bu.edu.eg>
2024-11-02 12:22:23 +0200

    Update extract_frames_util.py

--- utils/extract_frames_util.py
+++ utils/extract_frames_util.py
@@ -32,18 +32,19 @@ def extract_frames(video_path: str, output_folder: str, frame_interval: int) -> 
             break
         
         if frame_number % interval == 0:
-            frame_filename = os.path.join(output_folder, f"frame_{saved_frame_count}.jpg")
-            cv2.imwrite(frame_filename, frame)
+            frame_filename = os.path.join(output_folder, f"frame_{saved_frame_count}.png")
+            cv2.imwrite(frame_filename, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
             saved_frame_count += 1
         
         frame_number += 1
+        print(f"Processing Frame: {frame_number}")
 
     video.release()
 
 if __name__ == '__main__':
 
-    video_path = 'ezz-avarol\ezz_workspace.mp4'
-    output_folder = 'output_frames'
+    video_path = 'D:\Artificial Intelligence\Machine Learning\Computer Vision\P_DeepPPP\data\match.mp4'
+    output_folder = 'D:\Artificial Intelligence\Machine Learning\Computer Vision\P_DeepPPP\data'
     frame_interval = 5  # seconds
 
     extract_frames(video_path, output_folder, frame_interval)
